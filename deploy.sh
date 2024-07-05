fa56600b4a95eb25f2d9d4259c521ebdee5d4905
Tristan Simpson <heytristaann@gmail.com>
2024-07-04 22:31:30 -0400

    c

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ ACR_IMAGE_VERSION="v1.0.0" # This can be anything you want
 RESOURCE_GROUP="change-me" # This is the name of your resource group
 FUNCTION_APP_NAME="change-me" # This is the name of your function app
 
+
 # Login to the ACR registry
 az acr login --name ${ACR_REGISTRY_NAME}
 
@@ -21,8 +22,10 @@ docker push ${ACR_LOGIN_SERVER}/${ACR_IMAGE_NAME}:${ACR_IMAGE_VERSION}
 # Enable admin user
 az acr update -n ${ACR_REGISTRY_NAME} --admin-enabled true
 
+
 # Get the password
 ACR_PASSWORD=$(az acr credential show --name ${ACR_REGISTRY_NAME} --query "passwords[0].value" --output tsv)
+
 
 # This will update the deployment with the new image
 az functionapp config container set --image ${ACR_IMAGE_NAME} --registry-password ${ACR_PASSWORD} --registry-username ${ACR_REGISTRY_NAME} --name ${FUNCTION_APP_NAME} --resource-group ${RESOURCE_GROUP}
